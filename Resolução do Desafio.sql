10c909633abbd9f9cb8528c1546727b12c9826ae
David Melo <124303430+davidmelo84@users.noreply.github.com>
2024-05-02 22:49:02 -0300

    Add files via upload

---
+++ Resolução do Desafio.sql
@@ -0,0 +1,149 @@
+--1
+
+SELECT 
+   Nome,
+   Ano
+
+FROM Filmes
+
+--2 
+
+SELECT 
+   *
+
+FROM Filmes
+ORDER BY Ano ASC
+
+--3 
+
+SELECT 
+   Nome,
+   Ano,
+   Duracao
+ 
+FROM Filmes
+WHERE Nome = 'De Volta para o Futuro';
+
+--4
+
+SELECT 
+* 
+FROM Filmes
+WHERE Ano = 1997
+
+--5
+
+SELECT
+*
+FROM Filmes
+WHERE ano > 2000
+
+--6
+
+SELECT
+
+* 
+FROM Filmes
+WHERE Duracao > 100 AND Duracao < 150 
+ORDER BY Duracao ASC
+
+-- 7
+
+SELECT
+   Ano,
+   COUNT(*) AS Quantidade
+FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC
+
+--8
+
+SELECT 
+  Id,
+  PrimeiroNome,
+  UltimoNome,
+  Genero
+ 
+FROM Atores
+WHERE Genero = 'M'
+
+
+--9
+
+SELECT 
+ Id,
+ PrimeiroNome,
+ UltimoNome,
+ Genero
+
+FROM Atores
+WHERE Genero = 'F'
+ORDER BY PrimeiroNome ASC
+
+--10
+
+SELECT 
+  Filmes.Nome,
+  Generos.Genero
+FROM 
+  Filmes
+JOIN 
+  FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+JOIN 
+  Generos ON FilmesGenero.IdGenero = Generos.Id;
+
+--11
+
+SELECT 
+  Filmes.Nome,
+  Generos.Genero
+FROM 
+  Filmes
+JOIN 
+  FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+JOIN 
+  Generos ON FilmesGenero.IdGenero = Generos.Id
+WHERE Generos.Genero = 'Mistério'
+
+--12 
+
+SELECT 
+ Filmes.Nome,
+ Atores.PrimeiroNome,
+ Atores.UltimoNome,
+ ElencoFilme.Papel
+
+FROM 
+Filmes
+
+JOIN 
+
+ElencoFilme ON Filmes.Id = ElencoFilme.Id
+
+JOIN
+
+Atores ON ElencoFilme.IdAtor = Atores.Id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
